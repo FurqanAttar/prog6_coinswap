08bbf93d853709e4bde81c8fa3d718baa7180c9c
Furqan Attar <SumiFurqan@me.att.net>
2016-11-27 13:06:31 -0600

    Initial Commit

---
+++ prog6fattar2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  prog6fattar2
+//
+//  Created by Furqan Attar on 11/27/16.
+//  Copyright © 2016 Furqan Attar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
